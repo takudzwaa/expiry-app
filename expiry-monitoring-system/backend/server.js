227ec235a53266eafe580693fa70b537119c22a1
Takudzwa Gondo <tcflsoftware@gmail.com>
2024-07-31 11:06:35 +0000

    Added server.js

---
+++ expiry-monitoring-system/backend/server.js
@@ -0,0 +1,43 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const itemSchema = new mongoose.Schema({
+    name: String,
+    expiryDate: Date,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.get('/items', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+app.post('/items', async (req, res) => {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.json(newItem);
+});
+
+app.put('/items/:id', async (req, res) => {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedItem);
+});
+
+app.delete('/items/:id', async (req, res) => {
+    await Item.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Item deleted' });
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
